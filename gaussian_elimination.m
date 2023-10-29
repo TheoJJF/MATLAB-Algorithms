97d5f25562850905dd6323b242d25dea8ed06c25
TheoJJF <131817438+TheoJJF@users.noreply.github.com>
2023-10-28 19:52:19 -0700

    Inital Commit(Gaussian Elimination-No Pivoting)

---
+++ gaussian_elimination.m
@@ -0,0 +1,26 @@
+function x = gaussian_elimination(A,b)
+    n = size(A,1);
+
+    if ((size(A,2) ~= n) || (size(b,1) ~= n) || (size(b,2) ~= 1))
+        error('Mismatch Size');
+    end
+
+    for i = 1:n
+        if (A(i,i) == 0)
+            error('Requires Partial Pivoting')
+        end
+
+        for j = (i+1):n
+            l = A(j,i) / A(i,i);
+            A(i,i) = 0;
+
+            for k = (i+1):n
+                A(j,k) = A(j,k) - l  * A(i,k);
+            end
+            b(j) = b(j) - l * b(i);
+        end
+        U = A;
+
+        x = backward_substitution(U,b);
+    end
+end
