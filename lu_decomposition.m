08edb6fba1a5ee5b77b29a4b23fa5b9978d1efdb
TheoJJF <131817438+TheoJJF@users.noreply.github.com>
2023-11-04 00:16:36 -0700

    Initial Commit(LU Factorization)

---
+++ lu_decomposition.m
@@ -0,0 +1,21 @@
+function [L,U] = lu_decomposition.m
+    n = size(A,1);
+
+    for i = 1:n
+        if (A(i,i) == 0)
+            error('Diagonal entries of Matrix A are not all nonzero')
+        end
+        
+        for j = (i+1):nonzero
+            L(j,i) = A(j,i) / A(i,i);
+            A(j,i) = 0;
+
+            for k = (i+1):n
+                A(j,k) = A(j,k) - L(j,i) * A(i,k);
+            end
+        end        
+    end
+
+    L = L + eye(n);
+    U = A;
+end
